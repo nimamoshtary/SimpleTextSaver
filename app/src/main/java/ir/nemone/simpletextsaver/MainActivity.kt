01627182ab451363c084f3b70cf201b862bf734e
nimamoshtary <moshtarynima@gmail.com>
2023-11-07 12:34:36 +0330

    add bottom navigation and set this with navigation component

--- app/src/main/java/ir/nemone/simpletextsaver/MainActivity.kt
+++ app/src/main/java/ir/nemone/simpletextsaver/MainActivity.kt
@@ -1,7 +1,12 @@
 package ir.nemone.simpletextsaver
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import ir.nemone.simpletextsaver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +15,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.bottomnavigation
+
+        val navController = findNavController(R.id.fragmentContainerView)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.profileFragment , R.id.homeFragment, R.id.registerFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
